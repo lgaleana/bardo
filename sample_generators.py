98d7bade82f3dabb89cfa9b97e89f4615be8e8d1
Luis Galeana <lsgaleana@gmail.com>
2020-03-25 12:22:25 -0600

    Removed unused structure

--- sample_generators.py
+++ sample_generators.py
@@ -4,16 +4,6 @@ from sklearn.utils import shuffle
 
 RANDOM_STATE = 0
 
-class Data:
-  def __init___(self, data, X, y, X_train, X_test, y_train, y_test):
-    self.data = data
-    self.X = X
-    self.y = y
-    self.X_train = X_train
-    self.X_test = X_test
-    self.y_train = y_train
-    self.y_test = y_test
-
 class SampleGen:
   def __init__(self, dataset, test_size):
     self.data = np.loadtxt(dataset, delimiter=',')
