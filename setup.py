b254f166eacfea9226195f458f452a0e46adb756
Luis Galeana <lsgaleana@gmail.com>
2020-04-17 10:14:02 -0500

    Updated setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-  name='app',
-  version='1.0.0',
+  name='bardo',
+  version='0.0.1',
   packages=find_packages(),
   zip_safe=False,
   package_data={
-    'app': ['templates/*', 'static/*', 'utils/*'],
+    'bardo': ['templates/*', 'static/*', 'utils/*'],
   },
   install_requires=[
     'flask',
