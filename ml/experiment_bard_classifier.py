caba0a07bcf005567bf48673263e734d33972d6e
Luis Galeana <lsgaleana@gmail.com>
2020-06-18 21:55:22 -0500

    Made changes for easier experimentation

--- ml/experiment_bard_classifier.py
+++ ml/experiment_bard_classifier.py
@@ -3,28 +3,42 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 import ml.sample_generators as s
 import ml.train_utils as t
+import bardo.utils.db_utils as db
 from datetime import datetime
 from copy import deepcopy
 import numpy as np
 
 
 ### Experimentation configs
-# Generators generate different training samples
-# We want to test many
 TEST_SIZE = 0.25
 K = 5
+# Train:test datasets
 ROOT = 'data/datasets'
+SUFFIX = 'test'
 datasets = [
-  f'{ROOT}/lsgaleana-gmail_com_test.txt',
-  f'{ROOT}/sheaney-gmail_com_test.txt',
-  f'{ROOT}/others_test.txt',
+  (['sheaney@gmail.com'], [
+    'sheaney@gmail.com',
+    'others'
+  ]),
 ]
+# Generators sample the data
 generators = [
-  s.BinaryTestGen(neg_train={1, 2, 6}),
-  s.BinaryTestGen({3, 4, 5}, {1, 2, 6}),
-  s.BinaryTestGen(neg_train={1, 2, 3, 6}),
-]
+  s.BinaryTestGen(),
+  s.BinaryTestGen({3, 4, 5}),
+  s.BinaryTestGen(neg_train={1, 2, 3}),
 
+  s.VeryBinaryTestGen(),
+  s.VeryBinaryTestGen(very=-1),
+  s.VeryBinaryTestGen(very=1),
+
+  s.VeryBinaryTestGen({3, 4, 5}),
+  s.VeryBinaryTestGen({3, 4, 5}, very=-1),
+  s.VeryBinaryTestGen({3, 4, 5}, very=1),
+
+  s.VeryBinaryTestGen(neg_train={1, 2, 3}),
+  s.VeryBinaryTestGen(neg_train={1, 2, 3}, very=-1),
+  s.VeryBinaryTestGen(neg_train={1, 2, 3}, very=1),
+]
 # CV parameters
 lsp = [{
   'C': [0.1, 1, 10, 100, 1000],
@@ -44,8 +58,7 @@ gp = [{
   'n_estimators': [16, 32, 64, 100, 150, 200],
   'learning_rate': [0.0001, 0.001, 0.01, 0.025, 0.05,  0.1, 0.25, 0.5],
 }]
-
-# Define what to experiment with
+# Models
 exp_configs = [
   {
     'name': 'Linear SVC',
@@ -79,76 +92,71 @@ exp_configs = [
 
 
 ### Run experiments
-now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-log_file = open(f'data/reports/{now}.txt', 'w+')
-log_file.write(',Train Acc,TestAcc,Test 1 Pr,Test 1 Rec,Test 0 Pr,Test 0 Rec\n')
-
-models = {}
-# Create all train configs
-# One per generator, config, config mode and dataset
-# (plus one with all datasets)
-for generator in generators:
-  print(generator.get_name())
+# Experiment configs
+# One per training dataset, generator, config and config mode
+metrics = {}
+# Train users map to datasets
+for train_users, test_users in datasets:
   t.print_line()
 
-  models[generator] = []
-  for config in exp_configs:
-    for mode in config['modes']:
-      all_generators = []
-      for dataset in datasets:
-        chunks = dataset.split('/')
-        user = chunks[len(chunks) - 1]
-        user_generator = generator.set(dataset)
+  train_files = [db.escape_bardo_id(user) for user in train_users]
+  train_datasets = [f'{ROOT}/{file_}_{SUFFIX}.txt' for file_ in train_files]
 
-        models[generator].append(t.TrainUtil(
-          name=f'{config["name"]}+{user}',
+  # Models contains a map from generator -> (model_name, (model, cv))
+  models = {}
+  for generator in generators:
+    gen_name = generator.get_name()
+    print(gen_name)
+    t.print_line()
+  
+    models[gen_name] = {}
+    for config in exp_configs:
+      for mode in config['modes']:
+        model_name = f'{config["name"]}:{"+".join(train_users)}'
+        tu = t.TrainUtil(
+          name=model_name,
           model=deepcopy(config['model']),
-          datasets=[deepcopy(user_generator)],
+          datasets=[deepcopy(generator).set(d) for d in train_datasets],
           test_size=TEST_SIZE,
           standardize=mode['standardize'],
           k=K,
           params=mode['params'],
-        ).train())
-        all_generators.append(deepcopy(user_generator))
+        )
 
-      models[generator].append(t.TrainUtil(
-        name=f'{config["name"]}+ALL',
-        model=deepcopy(config['model']),
-        datasets=all_generators,
-        test_size=TEST_SIZE,
-        standardize=mode['standardize'],
-        k=K,
-        params=mode['params'],
-      ).train())
-  t.print_line()
+        # We store an instance of model (for later) and its CV metrics
+        models[gen_name][model_name] = (tu.train(), tu.get_cv_metrics())
+    t.print_line()
 
-# Add a fictitional dataset, which is cross-validation, for metrics purpose
-datasets.insert(0, 'CV')
-# Experiment with train configs
-print('Generating metrics')
+  test_files = [db.escape_bardo_id(user) for user in test_users]
+  test_datasets = [f'{ROOT}/{file_}_{SUFFIX}.txt' for file_ in test_files]
+
+  print('Generating metrics')
+  # Test users map to test datasets
+  for test_user, test_dataset in zip(test_users, test_datasets):
+    if test_user not in metrics:
+      metrics[test_user] = {}
+
+    for gen_name, tus in models.items():
+      if gen_name not in metrics[test_user]:
+        metrics[test_user][gen_name] = {}
+
+      for model_name, tu in tus.items():
+        if test_user == 'CV':
+          metrics[test_user][gen_name][model_name] = tu[1]
+        else:
+          # We need the model instances to generate dataset-specific metrics
+          metrics[test_user][gen_name][model_name] = \
+            tu[0].get_test_metrics(deepcopy(generator).set(test_dataset))
+
+print('Writing metrics')
 t.print_line()
-for dataset in datasets:
-  if dataset == 'CV':
-    print(f'CV')
-    log_file.write('CV\n')
-  else:
-    chunks = dataset.split('/')
-    user = chunks[len(chunks) - 1]
-    print(f'{user}')
-    log_file.write(f'{user}\n')
-  t.print_line()
-
-  for generator, tus in models.items():
-    print(f'{generator.get_name()}')
-    log_file.write(f'{generator.get_name()}\n')
-    for tu in tus:
-      if dataset == 'CV':
-        cv = tu.get_cv_metrics()
-        log_file.write(f'{tu.get_name()},{cv["train_acc"]},{cv["test_acc"]},{cv["test_pr_1"]},{cv["test_rec_1"]},{cv["test_pr_0"]},{cv["test_rec_0"]}\n')
-      else:
-        tm = tu.get_test_metrics(deepcopy(generator).set(dataset))
-        log_file.write(f'{tu.get_name()},{tm["train_acc"]},{tm["test_acc"]},{tm["test_pr_1"]},{tm["test_rec_1"]},{tm["test_pr_0"]},{tm["test_rec_0"]}\n')
-  log_file.write('\n')
-  t.print_line()
-log_file.close()
+now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+with open(f'data/reports/{now}.txt', 'w+') as log_file:
+  log_file.write(',,Train Acc,TestAcc,Test 1 Pr,Test 1 Rec,Test 0 Pr,Test 0 Rec\n')
+  for test_user, models in metrics.items():
+    log_file.write(f'{test_user}\n')
+    for generator, tm in models.items():
+      log_file.write(f'{generator}')
+      for model_name, m in tm.items():
+        log_file.write(f',{model_name},{m["train_acc"]},{m["test_acc"]},{m["test_pr_1"]},{m["test_rec_1"]},{m["test_pr_0"]},{m["test_rec_0"]}\n')
 print('Finished training')
